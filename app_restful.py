e141618014d34e8561e482b8363000fc79fd3083
Ahmed Soliman <ahmedsoliman202@gmail.com>
2023-08-23 14:00:13 +0300

    create simple api using flaskRESTful module

---
+++ app_restful.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+class Item(Resource):
+    def get(self,name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item':None} , 404
+    
+    def post(self,name):
+        request_data = request.get_json()
+        item = {'name':name, 'price':request_data['price']}
+        items.append(item)
+        return item , 201
+    
+class Items(Resource):
+    def get(self):
+        return {'items' : items}
+    
+
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(Items,'/items')
+app.run(port=5000,debug=True)
