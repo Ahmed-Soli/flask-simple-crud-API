ed57a333be79d3936a1750d80e0d65937aaf975b
Ahmed Soliman <ahmedsoliman202@gmail.com>
2023-08-23 20:05:53 +0300

    change the logic for create item to use the sqllite db instead of in memory db

--- item.py
+++ item.py
@@ -8,6 +8,13 @@ class Item(Resource):
 
     @jwt_required()
     def get(self,name):
+        item = Item.find_item_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+    
+    @classmethod
+    def find_item_by_name(cls,name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -18,14 +25,22 @@ class Item(Resource):
         
         if row:
             return {'item' : {'name':row[0], 'price':row[1]}}
-        return {'message': 'Item not found'}, 404
-    
+
     def post(self,name):
-        if next(filter(lambda element : element['name'] == name , items),None) :
+        item = Item.find_item_by_name(name)
+        if item:
             return {'message' : 'an item with name {} already exists'.format(name)} , 400
         request_data = Item.parser.parse_args()
         item = {'name':name, 'price':request_data['price']}
-        items.append(item)
+        
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query,(item['name'],item['price']))
+        connection.commit()
+        connection.close()
+        
         return item , 201
     
     def delete(self,name):
