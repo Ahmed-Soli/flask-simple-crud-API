69147654d84f99d5df12f96629853a1b57d62550
Ahmed Soliman <ahmedsoliman202@gmail.com>
2023-08-24 12:36:39 +0300

    setting up SQLAlchemy

--- app_restful.py
+++ app_restful.py
@@ -6,6 +6,8 @@ from resources.user import UserRegistration
 from resources.item import Item, Items
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ahmed'
 api = Api(app)
 
@@ -17,4 +19,6 @@ api.add_resource(Items,'/items')
 api.add_resource(UserRegistration,'/register')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5000,debug=True)
