2b5c5d07a1335b45b887ea44cd7c4ef64855353d
Ahmed Soliman <ahmedsoliman202@gmail.com>
2023-08-24 11:07:03 +0300

    refactored the stucture of the app module

--- app_restful.py
+++ app_restful.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask_restful import  Api
+from flask_jwt import JWT
 from security import authenticate, identity
 from user import UserRegistration
+from item import Item, Items
 
 app = Flask(__name__)
 app.secret_key = 'ahmed'
@@ -10,46 +11,10 @@ api = Api(app)
 
 jwt = JWT(app,authenticate, identity) # creates /auth and takes username and password
 
-items = []
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',type=float,required=True,help='This field can not be left blank!')
-
-    @jwt_required()
-    def get(self,name):
-        item = next(filter(lambda element : element['name'] == name , items),None)
-        return {'item':item} , 200 if item else 404
-    
-    def post(self,name):
-        if next(filter(lambda element : element['name'] == name , items),None) :
-            return {'message' : 'an item with name {} already exists'.format(name)} , 400
-        request_data = Item.parser.parse_args()
-        item = {'name':name, 'price':request_data['price']}
-        items.append(item)
-        return item , 201
-    
-    def delete(self,name):
-        global items
-        items = list(filter(lambda elem: elem['name'] != name, items))
-        return {'message': f'Item {name} Deleted'}
-
-    def put(self,name):        
-        item = next(filter(lambda element : element['name'] == name , items),None)
-        request_data = Item.parser.parse_args()
-        if item is None: # Create new item
-            item = {'name' : name, 'price' : request_data['price']}
-            items.append(item)
-        else: # Update the item
-            item.update(request_data)
-        return item
-
-    
-class Items(Resource):
-    def get(self):
-        return {'items' : items}
-    
 
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(Items,'/items')
 api.add_resource(UserRegistration,'/register')
-app.run(port=5000,debug=True)
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
